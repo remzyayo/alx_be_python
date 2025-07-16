450b3a96a28e395a115433ea22077d578304491a
REMZY <oluremifrancisaya@gmail.com>
2025-07-16 05:09:57 -0700

    Update weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-weather = input("What's the weather like today? ").lower()
+weather = input("What's the weather like today?: ").lower()
 if weather == "sunny": 
   print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
