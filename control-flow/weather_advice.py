763bcd9cb7a45dbbf46a4a69bf2d4bffe49c095a
Francis <oluremifrancisaya@gmailcom>
2025-07-16 13:07:27 -0700

    match_case_calculator

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
-weather_input = input("What's the weather like today? (sunny, rainy, cold): ").lower()
-if weather_input == "sunny": 
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+if weather == "sunny": 
   print("Wear a t-shirt and sunglasses.")
-elif weather_input == "raining":
+elif weather == "rainy":
   print("Don't forget your umbrella and a raincoat.")
-elif weather_input == "cold":
+elif weather == "cold":
   print("Make sure to wear a warm coat and a scarf.")
 else:
-  print(" Sorry, I don't have recommendations for this weather.")
+  print("Sorry, I don't have recommendations for this weather.")
 
