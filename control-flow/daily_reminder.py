a039d022f7c45a81c63ac6d2c0b16875be64ddd3
Francis <oluremifrancisaya@gmailcom>
2025-07-17 21:42:35 -0700

    daily_reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,17 +1,16 @@
 Task = input("Enter your task: ")
 Priority = input("Priority (high/medium/low): ").lower()
 TimeBound = input("Is it time-bound? (yes/no): ").lower()
-match priority:
+match Priority:
     case "high":
-        reminder = f" High Priority: Don't forget to {task}."
+        reminder = f" High Priority: Don't forget to {Task}."
     case "medium":
-        reminder = f" Medium Priority: Make time to {task}."
+        reminder = f" Medium Priority: Make time to {Task}."
     case "low":
-        reminder = f" Low Priority: You can do {task} when you have free time."
+        reminder = f" Low Priority: You can do {Task} when you have free time."
     case _:
-        reminder = f" Unknown Priority: {task} (check the priority spelling)."
+        reminder = f" Unknown Priority: {Task} (check the priority spelling)."
 if TimeBound == "yes":
     reminder += " This is a time-sensitive task that requires immediate attention today!"
 print("\n REMINDER")
 print(reminder)
-         
