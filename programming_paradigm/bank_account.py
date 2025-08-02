c2813608ccaa70755a7367a94e3df6bac898eed7
REMZY <oluremifrancisaya@gmail.com>
2025-08-02 04:05:56 -0700

    Update bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+# bank_account.py
+class BankAccount:
+    def _init_(self, initial_balance=0):
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        print(f"Current balance: ${self.__account_balance:.2f}")
