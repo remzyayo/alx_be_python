10a69a2dd20e4f9678fd1c4c61a44255aff093bc
REMZY <oluremifrancisaya@gmail.com>
2025-08-02 04:50:03 -0700

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,5 +14,6 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current balance: ${self.__account_balance:.2f}")
+        print(f"Current Balance: ${self.__account_balance:.2f}")
+
 
