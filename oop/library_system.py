347c6edb1ad8a945a35d1852308f761051ac9200
REMZY <oluremifrancisaya@gmail.com>
2025-08-09 01:11:42 -0700

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -18,7 +18,7 @@ class EBook(Book):
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
-        super()._init_(title, author)
+        super().__init__(title, author)
         self.page_count = page_count
 
     def __str__(self):
@@ -35,4 +35,5 @@ class Library:
     def list_books(self):
         for book in self.books:
             print(book)
+
 
