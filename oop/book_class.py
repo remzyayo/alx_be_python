6768d0b9168113701d1fc59145ad5dc5d4face32
REMZY <oluremifrancisaya@gmail.com>
2025-08-09 00:54:20 -0700

    book_class.py

---
+++ oop/book_class.py
@@ -0,0 +1,15 @@
+# book_class.py
+class Book:
+    def _init_(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def _del_(self):
+        print(f"Deleting {self.title}")
+
+    def _str_(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def _repr_(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
